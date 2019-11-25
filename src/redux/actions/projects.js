fa3bf81c7995af395efba8dfeeb7bfd18d992e62
Brett Green <me@brettmichaelgreen.com>
2019-11-25 05:28:13 -0500

    Updates

--- src/redux/actions/projects.js
+++
@@ -1,5 +0,0 @@
-import { prefixes, actionTypes } from "./_constants"
-import getAsyncAction from "./asyncActionUtils"
-import apis from "../../api/projects"
-
-export const fetchProjects = getAsyncAction({ actionTypePrefix: prefixes.PROJECTS+actionTypes.FETCH, asyncFunc: apis.fetchProjects });
--- src/redux/reducers/initialState.js
+++
@@ -1,9 +0,0 @@
-import { List } from "immutable"
-
-export default {
-    projects: new List(),
-    socials: new List(),
-
-    isFetchingProjects: false,
-    isFetchingSocials: false,
-}
