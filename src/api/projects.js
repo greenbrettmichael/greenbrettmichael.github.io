bce71da29f10f0dda20d91448597e6a05a110431
Brett Green <me@brettmichaelgreen.com>
2019-11-25 04:21:32 -0500

    add actual pictures for WOWQueueCamper project

--- src/api/projects.js
+++ src/api/projects.js
@@ -8,8 +8,8 @@ export default {
             links: [ { url: "https://github.com/greenbrettmichael/WOWQueueCamper", name: "Open project", icon: "fab fa-github"} ],
             secondaryLinks: [],
             labels: [ "cv" ],
-            imgSmall: "https://firebasestorage.googleapis.com/v0/b/greenbrettmichael-github-io.appspot.com/o/profile.png?alt=media&token=92d9a5e0-b6a2-481e-a44d-bfcecb386b77",
-            img: "https://firebasestorage.googleapis.com/v0/b/greenbrettmichael-github-io.appspot.com/o/profile.png?alt=media&token=92d9a5e0-b6a2-481e-a44d-bfcecb386b77",
+            imgSmall: "https://firebasestorage.googleapis.com/v0/b/greenbrettmichael-github-io.appspot.com/o/WOWQueueCamper_small.png?alt=media&token=ebbffdf2-b412-42db-9ea9-5b3e38d9b36a",
+            img: "https://firebasestorage.googleapis.com/v0/b/greenbrettmichael-github-io.appspot.com/o/WOWQueueCamper.png?alt=media&token=9e261130-7c2e-4780-9f2a-1d34f325a6c1",
             highlight: true
         }
          ] ) )
