f84f9c7a9365b11891b9ee487964a323bab06807
Brett Green <me@brettmichaelgreen.com>
2019-11-25 05:04:28 -0500

    hard route for one page

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -7,24 +7,6 @@ import Home from './home/Home'
 
 import ReactGA from 'react-ga';
 
-const routes = [
-    {
-        name: "Home",
-        path: "/",
-        // exact: true,
-        component: Home
-    }, 
-    // {
-    //     name: "Project Details",
-    //     path: "/:projectId",
-    //     component: ProjectDetails
-    // },
-    // {
-    //     name: "404 No match",
-    //     component: PageNotFound
-    // }
-];
-
 const Routes = () => {    
     
     const logPageView = () => {
@@ -36,7 +18,7 @@ const Routes = () => {
     return (
         <Router basename={process.env.PUBLIC_URL}> 
             <Switch >               
-                { routes.map( route => <Route key={route.name} exact={route.exact} path={route.path} component={route.component} /> ) }
+            <Route exact path="/" component={Home} />
             </Switch>
         </Router>
     );
